joker-eph/mlir-filtered

commit 2427ef1af59b6c249e08fd967d62d96f7fc34569

DBG_VALUE machine instruction pointing to undefined register for a variable justify a separate scope if the variable is inlined function's argument. Radar 8122864.
llvm-svn: 106792

llvm/lib/CodeGen/AsmPrinter/DwarfDebug.cpp
@@ -2390,8 +2390,6 @@ DbgScope *DwarfDebug::getOrCreateDbgScope(const MDNode *Scope, const MDNode *Inl
 static bool hasValidLocation(LLVMContext &Ctx,
                              const MachineInstr *MInsn,
                              const MDNode *&Scope, const MDNode *&InlinedAt) {
-  if (MInsn->isDebugValue())
-    return false;
   DebugLoc DL = MInsn->getDebugLoc();
   if (DL.isUnknown()) return false;
       
@@ -2655,7 +2653,6 @@ void DwarfDebug::beginFunction(const MachineFunction *MF) {
         assert (MI->getNumOperands() > 1 && "Invalid machine instruction!");
         DIVariable DV(MI->getOperand(MI->getNumOperands() - 1).getMetadata());
         if (!DV.Verify()) continue;
-        if (isDbgValueInUndefinedReg(MI)) continue;
         // If DBG_VALUE is for a local variable then it needs a label.
         if (DV.getTag() != dwarf::DW_TAG_arg_variable)
           InsnNeedsLabel.insert(MI);
@@ -2663,7 +2660,7 @@ void DwarfDebug::beginFunction(const MachineFunction *MF) {
         else if (!DISubprogram(DV.getContext()).describes(MF->getFunction()))
           InsnNeedsLabel.insert(MI);
         // DBG_VALUE indicating argument location change needs a label.
-        else if (!ProcessedArgs.insert(DV))
+        else if (isDbgValueInUndefinedReg(MI) == false && !ProcessedArgs.insert(DV))
           InsnNeedsLabel.insert(MI);
       } else {
         // If location is unknown then instruction needs a location only if 

llvm/test/FrontendC/2010-06-24-DbgInlinedFnParameter.c
@@ -1,0 +1,15 @@
+// RUN: %llvmgcc -S -O2 -g %s -o - | llc -O2 -o %t.s
+// RUN: grep  "# DW_TAG_formal_parameter" %t.s | count 4
+// Radar 8122864
+static int foo(int a, int j) {
+  int k = 0;
+  if (a)
+    k = a + j;
+  else
+    k = j;
+  return k;
+}
+int bar(int o, int p) {
+
+  return foo(o, p);
+}

commit 0253ec949e0e15a416cfbe37ecd6e384d2d79366

Do not require line number entry for undefined local variable. This is a regression caused by r106792 and caught by gdb testsuite.
llvm-svn: 107430

llvm/lib/CodeGen/AsmPrinter/DwarfDebug.cpp
@@ -2690,10 +2690,12 @@ void DwarfDebug::beginFunction(const MachineFunction *MF) {
         DIVariable DV(MI->getOperand(MI->getNumOperands() - 1).getMetadata());
         if (!DV.Verify()) continue;
         // If DBG_VALUE is for a local variable then it needs a label.
-        if (DV.getTag() != dwarf::DW_TAG_arg_variable)
+        if (DV.getTag() != dwarf::DW_TAG_arg_variable 
+            && isDbgValueInUndefinedReg(MI) == false)
           InsnNeedsLabel.insert(MI);
         // DBG_VALUE for inlined functions argument needs a label.
-        else if (!DISubprogram(DV.getContext()).describes(MF->getFunction()))
+        else if (!DISubprogram(getDISubprogram(DV.getContext())).
+                 describes(MF->getFunction()))
           InsnNeedsLabel.insert(MI);
         // DBG_VALUE indicating argument location change needs a label.
         else if (isDbgValueInUndefinedReg(MI) == false && !ProcessedArgs.insert(DV))