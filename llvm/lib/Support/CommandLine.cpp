joker-eph/mlir-filtered

commit 1dfe5845878ebb983f2fcd61e79531e2173264b0

Add an assertion for cl::opt names: they can't start with '-'
llvm-svn: 290467

llvm/lib/Support/CommandLine.cpp
@@ -373,6 +373,7 @@ void Option::removeArgument() { GlobalParser->removeOption(this); }
 void Option::setArgStr(StringRef S) {
   if (FullyInitialized)
     GlobalParser->updateArgStr(this, S);
+  assert(S[0] != '-' && "Option can't start with '-");
   ArgStr = S;
 }
 

commit 0ebbdcaf2db4ca43b67bf790f35f5669ed459083

CommandLine option: Relax the assertion introduced in r290467 to allows for empty string
This is used in LDC for custom boolean commandline options, setArgStr
is called with an empty string before using AddLiteralOption.

llvm-svn: 291406

llvm/lib/Support/CommandLine.cpp
@@ -373,7 +373,7 @@ void Option::removeArgument() { GlobalParser->removeOption(this); }
 void Option::setArgStr(StringRef S) {
   if (FullyInitialized)
     GlobalParser->updateArgStr(this, S);
-  assert(S[0] != '-' && "Option can't start with '-");
+  assert((S.empty() || S[0] != '-') && "Option can't start with '-");
   ArgStr = S;
 }
 