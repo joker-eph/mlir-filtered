joker-eph/mlir-filtered

commit 7cc14a661ec0789f4623332206ed0c90a910f63c

[OpaquePtr] Remove uses of CreateGEP() without element type
Remove uses of to-be-deprecated API. In cases where the correct
element type was not immediately obvious to me, fall back to
explicit getPointerElementType().

mlir/lib/ExecutionEngine/ExecutionEngine.cpp
@@ -179,7 +179,8 @@ static void packFunctionArguments(Module *module) {
     for (auto &indexedArg : llvm::enumerate(func.args())) {
       llvm::Value *argIndex = llvm::Constant::getIntegerValue(
           builder.getInt64Ty(), APInt(64, indexedArg.index()));
-      llvm::Value *argPtrPtr = builder.CreateGEP(argList, argIndex);
+      llvm::Value *argPtrPtr = builder.CreateGEP(
+          builder.getInt8Ty(), argList, argIndex);
       llvm::Value *argPtr = builder.CreateLoad(builder.getInt8PtrTy(),
                                                argPtrPtr);
       llvm::Type *argTy = indexedArg.value().getType();
@@ -195,7 +196,8 @@ static void packFunctionArguments(Module *module) {
     if (!result->getType()->isVoidTy()) {
       llvm::Value *retIndex = llvm::Constant::getIntegerValue(
           builder.getInt64Ty(), APInt(64, llvm::size(func.args())));
-      llvm::Value *retPtrPtr = builder.CreateGEP(argList, retIndex);
+      llvm::Value *retPtrPtr =
+          builder.CreateGEP(builder.getInt8Ty(), argList, retIndex);
       llvm::Value *retPtr = builder.CreateLoad(builder.getInt8PtrTy(),
                                                retPtrPtr);
       retPtr = builder.CreateBitCast(retPtr, result->getType()->getPointerTo());

llvm/lib/Target/AMDGPU/AMDGPUReplaceLDSUseWithPointer.cpp
@@ -302,7 +302,7 @@ class ReplaceLDSUseImpl {
     // Insert required set of instructions which replace LDS within F.
     auto *V = Builder.CreateBitCast(
         Builder.CreateGEP(
-            LDSMemBaseAddr,
+            Builder.getInt8Ty(), LDSMemBaseAddr,
             Builder.CreateLoad(LDSPointer->getValueType(), LDSPointer)),
         GV->getType());
 

commit f1936f75dcb3f754416a682a5446bdd3b4ab949e

[ExecutionEngine] Fix GEP type
Fix bug introduced in 7cc14a661ec0789f4623332206ed0c90a910f63c,
the GEP type was off-by-ptr. Apparently I didn't run the MLIR
tests.

mlir/lib/ExecutionEngine/ExecutionEngine.cpp
@@ -180,7 +180,7 @@ static void packFunctionArguments(Module *module) {
       llvm::Value *argIndex = llvm::Constant::getIntegerValue(
           builder.getInt64Ty(), APInt(64, indexedArg.index()));
       llvm::Value *argPtrPtr = builder.CreateGEP(
-          builder.getInt8Ty(), argList, argIndex);
+          builder.getInt8PtrTy(), argList, argIndex);
       llvm::Value *argPtr = builder.CreateLoad(builder.getInt8PtrTy(),
                                                argPtrPtr);
       llvm::Type *argTy = indexedArg.value().getType();
@@ -197,7 +197,7 @@ static void packFunctionArguments(Module *module) {
       llvm::Value *retIndex = llvm::Constant::getIntegerValue(
           builder.getInt64Ty(), APInt(64, llvm::size(func.args())));
       llvm::Value *retPtrPtr =
-          builder.CreateGEP(builder.getInt8Ty(), argList, retIndex);
+          builder.CreateGEP(builder.getInt8PtrTy(), argList, retIndex);
       llvm::Value *retPtr = builder.CreateLoad(builder.getInt8PtrTy(),
                                                retPtrPtr);
       retPtr = builder.CreateBitCast(retPtr, result->getType()->getPointerTo());