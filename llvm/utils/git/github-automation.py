joker-eph/mlir-filtered

commit 4f6b1e70cfee88656c3e9b400a081606ded90fc8

github: Fix release automation /branch command with new repo
We started using the llvm/llvm-project-release-prs repo for
backport pull requests, but since this repo is not a fork of
llvm/llvm-project it will reject pull requests from other repos. In
order to fix this, when ever someone uses the /branch command to request
a branch be merged into the release branch, we first copy the branch to
the llvm-project-release-prs repo and then create the pull request.

Reviewed By: thieta

Differential Revision: https://reviews.llvm.org/D126940

llvm/utils/git/github-automation.py
@@ -14,6 +14,7 @@ import github
 import os
 import re
 import sys
+import time
 from typing import *
 
 class IssueSubscriber:
@@ -224,7 +225,29 @@ class ReleaseWorkflow:
         release_branch_for_issue = self.release_branch_for_issue
         if release_branch_for_issue is None:
             return False
-        head = f"{owner}:{branch}"
+        head_branch = branch
+        if not repo.fork:
+            # If the target repo is not a fork of llvm-project, we need to copy
+            # the branch into the target repo.  GitHub only supports cross-repo pull
+            # requests on forked repos.
+            head_branch = f'{owner}-{branch}'
+            local_repo = Repo(self.llvm_project_dir)
+            push_done = False
+            for i in range(0,5):
+                try:
+                    local_repo.git.fetch(f'https://github.com/{owner}/llvm-project', f'{branch}:{branch}')
+                    local_repo.git.push(self.push_url, f'{branch}:{head_branch}', force=True)
+                    push_done = True
+                    break
+                except Exception as e:
+                    print(e)
+                    time.sleep(30)
+                    continue
+            if not push_done:
+                raise Exception("Failed to mirror branch into {}".format(self.push_url))
+            owner = repo.owner.login
+
+        head = f"{owner}:{head_branch}"
         if self.check_if_pull_request_exists(repo, head):
             print("PR already exists...")
             return True

commit 01d681305aea13a7c2d6b6a4df7cf8198c79bd66

workflows: Fix pull request creation for backports
This was broken by 4f6b1e70cfee88656c3e9b400a081606ded90fc8.

llvm/utils/git/github-automation.py
@@ -301,7 +301,7 @@ class ReleaseWorkflow:
         pulls = repo.get_pulls(head=head)
         return pulls.totalCount != 0
 
-    def create_pull_request(self, owner:str, branch:str) -> bool:
+    def create_pull_request(self, owner:str, repo_name:str, branch:str) -> bool:
         """
         reate a pull request in `self.branch_repo_name`.  The base branch of the
         pull request will be choosen based on the the milestone attached to
@@ -327,7 +327,7 @@ class ReleaseWorkflow:
             push_done = False
             for i in range(0,5):
                 try:
-                    local_repo.git.fetch(f'https://github.com/{owner}/llvm-project', f'{branch}:{branch}')
+                    local_repo.git.fetch(f'https://github.com/{owner}/{repo_name}', f'{branch}:{branch}')
                     local_repo.git.push(self.push_url, f'{branch}:{head_branch}', force=True)
                     push_done = True
                     break
@@ -392,8 +392,9 @@ class ReleaseWorkflow:
                 m = re.match('([^/]+)/([^/]+)/(.+)', args)
                 if m:
                     owner = m.group(1)
+                    repo = m.group(2)
                     branch = m.group(3)
-                    return self.create_pull_request(owner, branch)
+                    return self.create_pull_request(owner, repo, branch)
 
         print("Do not understand input:")
         print(sys.stdin.readlines())