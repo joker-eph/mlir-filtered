joker-eph/mlir-filtered

commit a4669c81d7304c3d9abcf0a23613a1c0561816b0

[MLIR] [Python] Fix `Value.owner` to handle BlockArgs
Previously, calling `Value.owner()` would C++ assert in debug builds if
`Value` was a block argument. Additionally, the behavior was just wrong
in release builds. This patch adds support for BlockArg Values.

mlir/lib/Bindings/Python/IRCore.cpp
@@ -3118,11 +3118,22 @@ void mlir::python::populateIRCore(py::module &m) {
       .def_property_readonly(
           "owner",
           [](PyValue &self) {
-            assert(mlirOperationEqual(self.getParentOperation()->get(),
-                                      mlirOpResultGetOwner(self.get())) &&
-                   "expected the owner of the value in Python to match that in "
-                   "the IR");
-            return self.getParentOperation().getObject();
+            MlirValue v = self.get();
+            if (mlirValueIsAOpResult(v)) {
+              assert(
+                  mlirOperationEqual(self.getParentOperation()->get(),
+                                     mlirOpResultGetOwner(self.get())) &&
+                  "expected the owner of the value in Python to match that in "
+                  "the IR");
+              return self.getParentOperation().getObject();
+            }
+
+            if (mlirValueIsABlockArgument(v)) {
+              MlirBlock block = mlirBlockArgumentGetOwner(self.get());
+              return py::cast(PyBlock(self.getParentOperation(), block));
+            }
+
+            assert(false && "Value must be a block argument or an op result");
           })
       .def("__eq__",
            [](PyValue &self, PyValue &other) {

mlir/test/python/ir/value.py
@@ -37,6 +37,21 @@ def testOpResultOwner():
     assert op.result.owner == op
 
 
+# CHECK-LABEL: TEST: testBlockArgOwner
+@run
+def testBlockArgOwner():
+  ctx = Context()
+  ctx.allow_unregistered_dialects = True
+  module = Module.parse(
+      r"""
+    func.func @foo(%arg0: f32) {
+      return
+    }""", ctx)
+  func = module.body.operations[0]
+  block = func.regions[0].blocks[0]
+  assert block.arguments[0].owner == block
+
+
 # CHECK-LABEL: TEST: testValueIsInstance
 @run
 def testValueIsInstance():

commit 21a3893ffd48d79b7dad948d5f3201a1e42af96b

[MLIR] [Python] Fix the Windows build broken by a4669c8
Windows builds require all control paths return. Since we don't have
`llvm_unreachable` in the Python bindings, just return `None`.

mlir/lib/Bindings/Python/IRCore.cpp
@@ -3117,7 +3117,7 @@ void mlir::python::populateIRCore(py::module &m) {
           kDumpDocstring)
       .def_property_readonly(
           "owner",
-          [](PyValue &self) {
+          [](PyValue &self) -> py::object {
             MlirValue v = self.get();
             if (mlirValueIsAOpResult(v)) {
               assert(
@@ -3134,6 +3134,7 @@ void mlir::python::populateIRCore(py::module &m) {
             }
 
             assert(false && "Value must be a block argument or an op result");
+            return py::none();
           })
       .def("__eq__",
            [](PyValue &self, PyValue &other) {