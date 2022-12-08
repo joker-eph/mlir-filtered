joker-eph/mlir-filtered

commit ebcc179705b7759f692190dffd14052749f4d740

Fix an unused-variable warning in release build, NFC

llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -3722,8 +3722,8 @@ struct AAIsDeadFloating : public AAIsDeadValueImpl {
       // still needed.
       if (auto *SI = dyn_cast<StoreInst>(I)) {
         SmallSetVector<Instruction *, 8> AssumeOnlyInst;
-        bool IsDead = isDeadStore(A, *SI, &AssumeOnlyInst);
-        assert(IsDead && "Store was assumed to be dead!");
+        assert(isDeadStore(A, *SI, &AssumeOnlyInst) &&
+               "Store was assumed to be dead!");
         A.deleteAfterManifest(*I);
         for (size_t i = 0; i < AssumeOnlyInst.size(); ++i) {
           Instruction *AOI = AssumeOnlyInst[i];

commit 26c72480076e2b8125378cedfb9730a24f7c8fb0

[Attributor] Fix a break in relase build caused by ebcc179705b7759f692190dffd14052749f4d740.

llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -3722,8 +3722,9 @@ struct AAIsDeadFloating : public AAIsDeadValueImpl {
       // still needed.
       if (auto *SI = dyn_cast<StoreInst>(I)) {
         SmallSetVector<Instruction *, 8> AssumeOnlyInst;
-        assert(isDeadStore(A, *SI, &AssumeOnlyInst) &&
-               "Store was assumed to be dead!");
+        bool IsDead = isDeadStore(A, *SI, &AssumeOnlyInst);
+        (void)IsDead;
+        assert(IsDead && "Store was assumed to be dead!");
         A.deleteAfterManifest(*I);
         for (size_t i = 0; i < AssumeOnlyInst.size(); ++i) {
           Instruction *AOI = AssumeOnlyInst[i];